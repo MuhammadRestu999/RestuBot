9cc37a015625cf48cf4dadeeae0de59a13604af7
Rey Sekha <gansrey97@gmail.com>
2021-11-08 00:12:50 +0700

    Fix simi simi

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,12 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   if (!text) throw 'Textnya apa?'
-  let res = await fetch(`https://api.zeks.me/api/simi?apikey=apivinz&text=${text}`)
+  let res = await fetch(`https://api-reysekha.herokuapp.com/api/fun/simisimi-ind2?text=${text}&apikey=apirey`)
   let json = await res.json()
   if (json.status !== true) throw json
-  m.reply(json.result)
+  m.reply(json.result.success)
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' [teks]')
+handler.help = ['simi', 'simi2', 'simsimi', 'simih'].map(v => v + ' [teks]')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 

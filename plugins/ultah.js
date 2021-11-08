bb468ae7adbb5d51c8f5a625a46b1114085f932d
Muhammad Restu <83255517+MuhammadRestu999@users.noreply.github.com>
2021-11-08 23:01:15 +0700

    Update ultah.js

--- plugins/ultah.js
+++ plugins/ultah.js
@@ -13,31 +13,31 @@ let tahun = now.getFullYear()
 // Handler
 let handler = async(m, { conn, text }) => {
   // Variable
-  let tttt = text.includes(" - ") ? text.split(" - ")[0] : text.split(" ")[0]
-  let bb = text.includes(" - ") ? text.split(" - ")[1] : text.split(" ")[1]
-  let tt = text.includes(" - ") ? text.split(" - ")[2] : text.split(" ")[2]
+  let tttt = text.includes(" - ") ? text.split(" - ")[0] : text.split("-")[0]
+  let bb = text.includes(" - ") ? text.split(" - ")[1] : text.split("-")[1]
+  let tt = text.includes(" - ") ? text.split(" - ")[2] : text.split("-")[2]
   tttt = Number(tttt)
   bb = Number(bb)
   tt = Number(tt)
 
   // Cek kalau ada yang invalid
-  if(tttt == NaN || tttt == undefined) return m.reply("Masukan tahun lahir!")
-  if(bb == NaN || bb == undefined) return m.reply("Masukan bulan lahir!")
-  if(tt == NaN || tt == undefined) return m.reply("Masukan tanggal lahir!")
+  if(tttt == NaN || tttt == undefined || tttt == 0) return m.reply("Masukan tahun lahir!")
+  if(bb == NaN || bb == undefined || bb == 0) return m.reply("Masukan bulan lahir!")
+  if(tt == NaN || tt == undefined || tt == 0) return m.reply("Masukan tanggal lahir!")
   if(isNaN(tttt)) return m.reply("Tahun harus angka!")
   if(isNaN(bb)) return m.reply("Bulan harus angka!")
   if(isNaN(tt)) return m.reply("Tanggal harus angka!")
   if(tt <= 0 || bb <= 0) return m.reply("Serius??")
 
   // Main
-  let umur = bulan >= bb && tanggal >= tt ? tahun - tttt : tahun - tttt - 1
-  let next = bulan >= bb && tanggal >= tt ? `${tahun + 1} - ${bb < 10 ? "0" + bb : bb} - ${tt < 10 ? "0" + tt : tt}` : `${tahun} - ${bb < 10 ? "0" + bb : bb} - ${tt < 10 ? "0" + tt : tt}`
+  let umur = bulan >= bb && tanggal >= tt ? tahun - tttt : bb < bulan ? tahun - tttt : tahun - tttt - 1
+  let next = bulan >= bb && tanggal >= tt ? `${tahun + 1}-${bb < 10 ? "0" + bb : bb}-${tt < 10 ? "0" + tt : tt}` : bb < bulan ? `${tahun + 1}-${bb < 10 ? "0" + bb : bb}-${tt < 10 ? "0" + tt : tt}` : `${tahun}-${bb < 10 ? "0" + bb : bb}-${tt < 10 ? "0" + tt : tt}`
 
   m.reply(`Umur saat ini : ${umur}\nUlang tahun berikutnya : ${next}`)
 }
 
-handler.help = ["ultah <tahun> <bulan> <tanggal>"]
+handler.help = ["ultah", "umur", "hitungumur"].map(v => v + " <tahun>-<bulan>-<tanggal>")
 handler.tags = ["other"]
-handler.command = /^(ultah)/i
+handler.command = /^(ultah|(hitung)?umur)/i
 
 module.exports = handler

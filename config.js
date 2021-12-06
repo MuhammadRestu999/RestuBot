0c3bfaf7378f8c154f9c2e179849fb78176def20
Reyレイ・セカ <gansrey97@gmail.com>
2021-12-06 13:08:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ global.mods = [] // moderator
 global.prems = []  //premium
 global.APIs = { // API Prefix
   // name: 'https://website'
-  rns: 'https://api-reysekha.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
   nzcha: 'http://nzcha-apii.herokuapp.com',
@@ -22,7 +22,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api-reysekha.herokuapp.com': 'apirey',
+  'https://server-api-rey.herokuapp.com': 'apirey',
   'https://api.xteam.xyz': 'APIKEY_DISINI',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://bsbt-api-rest.herokuapp.com': 'benniismael',

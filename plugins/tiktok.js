7a1b0cfa231fa03493550486f57a3bb8995c436d
Muhammad Restu <83255517+MuhammadRestu999@users.noreply.github.com>
2021-12-08 19:45:56 +0700

    Update tiktok.js
    global.wait :v

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,6 +1,5 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
   if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
   if (!args[0].match(/tiktok/gi)) throw `url salah`
 
@@ -8,10 +7,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   // if (!json.status) throw json
-  await m.reply(wait)
+  await m.reply(global.wait)
   await conn.sendFile(m.chat, json.result.nowatermark, '', `© RestuBot`, m)
-
 }
+
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 

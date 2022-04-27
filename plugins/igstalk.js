c27fe47e7e2485550aec8aa2bd1cb39d4b88c36e
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2022-04-27 13:51:03 +0700

    salah taro :v

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,34 +1,33 @@
 let axios = require("axios")
 
 let handler = async (m, { conn, args }) => {
-  if(!(global.owner.includes(m.sender.split("@")[0]) || global.mods.includes(m.sender.split("@")[0]))) return m.reply("Sedang dalam perbaikan!")
-  if (!args[0]) throw 'Uhm...username nya mana?'
+  if(!args[0]) return m.reply("Uhm...username nya mana?")
   let { data } = await axios(`https://hardianto.xyz/api/igstalk?username=${args[0]}&apikey=hardianto`)
-  if (data.code != 200) throw "Error!\n\nUsername tidak ditemukan / web api error"
+  if(data.message) throw data.message
   let {
-    fullname,
     username,
+    fullname,
     verified,
-    media_count,
-    follower_count,
-    following_count,
-    biography,
-    external_url,
-    profile_pic_url,
-    hd_profile_pic_url_info,
-    is_private
-  } = data.user
-  let pp = hd_profile_pic_url_info.url || profile_pic_url
+    video_count_reel,
+    followers,
+    follow,
+    is_bussines,
+    is_professional,
+    category,
+    thumbnail,
+    bio
+  } = data
+  let pp = thumbnail || false
   let caption = `
-${full_name} *(@${username})* ${is_verified ? '✓' : ''}
+${fullname} *(@${username})* ${verified ? '✓' : ''}
 https://instagram.com/${username}
-${is_private ? 'Post Hidden by User' : ('*' + media_count + '* Post(s)')}
-Following *${following_count}* User(s)
-*${follower_count}* Followers
+Reels : ${video_count_reel}
+Following *${follow}* User(s)
+*${followers}* Followers
 *Bio:*
-${biography}${external_url ? '\n*External URL:* ' + external_url : ''}
+${bio}
 `.trim()
-  if (pp) conn.sendFile(m.chat, pp, 'ppig.jpg', caption, m)
+  if(pp) conn.sendFile(m.chat, pp, 'ppig.jpg', caption, m)
   else m.reply(caption)
 }
 handler.help = ['igstalk'].map(v => v + ' <username>')
